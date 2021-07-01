c66a317fb5b4fbfafb03df85361bb9bb7f66d798
LSanchez17 <galaxyplayeremail@gmail.com>
2021-06-30 23:05:45 -0400

    initial set up

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+
+function App() {
+  //We need routes to turn this into a SPA
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/landingPage/Landing.js
@@ -0,0 +1,12 @@
+
+const Landing = () => {
+    return(
+        <div>
+            <h2>Welcome to Plant Keeper V2!</h2>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
+    );
+}
+
+export default Landing;
