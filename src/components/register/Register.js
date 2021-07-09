cf126d5317968a11c4804bbbc364cbd26ad748dc
LSanchez17 <galaxyplayeremail@gmail.com>
2021-07-08 23:57:06 -0400

    added register page

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,6 +1,25 @@
 const Register = () => {
     return (
-        <div></div>
+        <div>
+            <form>
+                <label htmlFor='name'>Name: </label>
+                <input type='text' />
+
+                <label htmlFor='email'>Email: </label>
+                <input type='email'/>
+
+                <label htmlFor='password'>Password: </label>
+                <input type='password' />
+
+                <label htmlFor='password2'>Confirm Password: </label>
+                <input type='password' />
+
+                <label htmlFor='zip'>Zip Code</label>
+                <input type='number' />
+
+                <button type='submit'>Register!</button>
+            </form>
+        </div>
     );
 };
 
