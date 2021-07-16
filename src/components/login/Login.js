d6a714af5d2d2f4a6bcb40f6b7f1fba571c2cf89
LSanchez17 <galaxyplayeremail@gmail.com>
2021-07-15 23:42:51 -0400

    styled login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,19 @@
 
 const Login = () => {
     return (
-        <div>
-            <form>
-                <label htmlFor='email'>Email:</label>
-                <input type='email' name='email' id='email' placeholder='Email...'></input>
+        <div className='bg-white p-3 m-20 rounded-lg'>
+            <form className='p-20 m-5 grid justify-items-center'>
+                <div>
+                    <label className='bg-yellow-400 bg-opacity-25 p-2 m-2' htmlFor='email'>Email: </label>
+                    <input className='bg-indigo-200 rounded-sm p-2 m-2' type='email' name='email' id='email' placeholder='Email...'></input>
+                </div>
 
-                <label htmlFor='password'>Password:</label>
-                <input type='password' name='password' id='password' placeholder='password...'></input>
+                <div>
+                    <label className='bg-yellow-400 bg-opacity-25 p-2 m-2' htmlFor='password'>Password: </label>
+                    <input className='bg-indigo-200 rounded-sm p-2 m-2' type='password' name='password' id='password' placeholder='password...'></input>
+                </div>
 
-                <button type='submit'>Login</button>
+                <button className='bg-green-200 p-2 m-2 rounded-sm' type='submit'>Login</button>
             </form>
         </div>
     );
