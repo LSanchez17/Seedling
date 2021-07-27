7b8ff37d832faa3af893ed6b5c2b578f184debf3
LSanchez17 <galaxyplayeremail@gmail.com>
2021-07-26 23:33:25 -0400

    weather section layout done

---
+++ src/components/Weather/DetailWeather.js
@@ -0,0 +1,18 @@
+
+const DetailWeather = ({forecast}) => {
+    //Get two weeks worth, and load as the user scrolls down
+
+    return(
+        <div>
+            <div>
+                <h3>Image goes here with corresponding emoji</h3>
+            </div>
+
+            <div>
+                <h3>Forecast notes go here, etc.</h3>
+            </div>
+        </div>
+    );
+};
+
+export default DetailWeather;
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,7 +1,25 @@
+import DetailWeather from './DetailWeather';
+import { useEffect, useState } from "react";
 
 const Weather = () => {
+    const [weather, setWeather] = useState();
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+
+            setWeather();
+        }
+    }, []);
+
+    let displayWeather = weather.map(day => {
+        <DetailWeather forecast={day} />
+    })
     return(
-        <div><h1>Display weather data here based on png file</h1></div>
+        <div>
+            <div>
+                {displayWeather ? displayWeather : <div><h3>Loading....</h3></div>}
+            </div>
+        </div>
     );
 };
 
